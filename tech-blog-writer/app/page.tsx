216384ce50c76eccae3a1b2adb05897be136f708
msaaddev <mrsaadirfan@gmail.com>
2024-04-18 23:43:39 +0200

    📦 NEW: Stream blog post

--- tech-blog-writer/app/page.tsx
+++ tech-blog-writer/app/page.tsx
@@ -1,11 +1,12 @@
 'use client';
 
-import { useState } from 'react';
-import { useChat } from 'ai/react';
+import { useEffect, useState } from 'react';
+import { useChat, useCompletion } from 'ai/react';
+import Message from './components/message';
 
 export default function Home() {
 	const [topic, setTopic] = useState('REST API Best Practices');
-	const [wordCount, setWordCount] = useState(750);
+	const [wordCount, setWordCount] = useState(500);
 	const [sentenceCount, setSentenceCount] = useState(4);
 	const [error, setError] = useState('');
 
@@ -33,7 +34,7 @@ export default function Home() {
 		setError('');
 		append({
 			role: 'user',
-			content: topic,
+			content: '',
 		});
 	};
 
@@ -51,6 +52,9 @@ export default function Home() {
 					setError('Internal server error. Refresh to try again. Or contact support.');
 				}
 			}
+		},
+		onFinish: message => {
+			console.log('onFinish', message);
 		},
 	});
 
@@ -128,9 +132,30 @@ export default function Home() {
 								<div className='flex gap-x-4'>
 									<button
 										type='submit'
-										className='inline-flex items-center justify-center rounded-lg text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 select-none cursor-pointer bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2'
+										className='inline-flex items-center justify-center rounded-lg text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 select-none cursor-pointer bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2 space-x-2'
 									>
-										{isLoading ? 'Generating...' : 'Generate'}
+										{isLoading && (
+											<svg
+												className='h-4 w-4 animate-spin mr-2'
+												viewBox='0 0 24 24'
+												fill='none'
+											>
+												<circle
+													className='opacity-25'
+													cx='12'
+													cy='12'
+													r='10'
+													stroke='currentColor'
+													stroke-width='4'
+												></circle>
+												<path
+													className='opacity-75'
+													fill='currentColor'
+													d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
+												></path>
+											</svg>
+										)}
+										{isLoading ? 'Generating' : 'Generate'}
 									</button>
 								</div>
 							</form>
@@ -155,7 +180,28 @@ export default function Home() {
 								</h2>
 							</div>
 						</div>
-						<div className='h-full overflow-scroll bg-muted border border-border-muted rounded-tl-lg rounded-bl-lg aspect-video shadow p-4 sm:p-2 lg:p-4'></div>
+						<div className='h-full overflow-scroll bg-muted border border-border-muted rounded-tl-lg rounded-bl-lg aspect-video shadow p-4 sm:p-2 lg:p-4'>
+							{error && (
+								<div className='text-red-500 italic'>
+									{error}
+								</div>
+							)}
+							{messages.length > 0 &&
+								messages
+									.filter(m => m.role !== 'user')
+									.slice(-1)
+									.map((m, index) => <Message key={index} message={m} />)}
+							{isLoading && !error.length && messages.length < 2 && (
+								<div className='text-muted-foreground italic'>
+									AI is thinking...
+								</div>
+							)}
+							{messages.length === 0 && !error && (
+								<div className='text-muted-foreground italic'>
+									No content yet. Generate a blog post to see the preview.
+								</div>
+							)}
+						</div>
 					</div>
 				</div>
 			</div>
