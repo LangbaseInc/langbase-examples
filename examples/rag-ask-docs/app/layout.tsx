b5f8f54f566d0c0607f6fb681e8e579746d66426
Saqib Ameen <saqib1@ualberta.ca>
2024-07-30 16:28:37 -0600

    👌 IMPROVE: add header

--- examples/rag-ask-docs/app/layout.tsx
+++ examples/rag-ask-docs/app/layout.tsx
@@ -2,6 +2,7 @@ import { cn } from 'mxcn';
 import type { Metadata } from 'next';
 import { Inter as FontSans } from 'next/font/google';
 import './globals.css';
+import { Header } from '@/components/ui/header';
 
 const fontSans = FontSans({
 	subsets: ['latin'],
@@ -26,6 +27,7 @@ export default function RootLayout({
 					fontSans.variable,
 				)}
 			>
+				<Header />
 				{children}
 			</body>
 		</html>
