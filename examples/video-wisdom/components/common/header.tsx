5b817c0d2992b7ada1364887282a15efa7ea4963
Saqib Ameen <saqib1@ualberta.ca>
2024-08-29 22:17:12 -0600

    👌 IMPROVE: add links

--- examples/video-wisdom/components/common/header.tsx
+++ examples/video-wisdom/components/common/header.tsx
@@ -3,10 +3,12 @@ import cn from 'mxcn'
 import Link from 'next/link'
 import { IconFork } from './../ui/iconists/icon-fork'
 import { IconGitHub } from './../ui/iconists/icon-github'
+import { Anchor } from '../ui/anchor'
+import { IconDocs } from '../ui/iconists/icon-docs'
 
 export async function Header() {
     return (
-        <header className="bg-background flex h-16 w-full items-center justify-between px-10 placeholder:backdrop:before:sm:px-80 py-16">
+        <header className="bg-background flex h-16 w-full items-center justify-between px-10 placeholder:backdrop:before:sm:px-80 py-10">
             <div className="flex h-16 shrink-0 items-center">
                 <h1>
                     <Link href="/" className="font-bold">
@@ -22,6 +24,18 @@ export async function Header() {
             </div>
 
             <div className="flex items-center justify-end space-x-2">
+
+                <Anchor
+                    variant="ghost"
+                    className="font-normal text-sm text-muted-foreground flex items-center"
+                    href="https://langbase.com/docs/guides/video-wisdom-extractor"
+                    target="_blank"
+                >
+                    <IconDocs className="size-4 text-muted-foreground/40" />
+                    <span className='hidden sm:flex'>Read How-To guide</span>
+                </Anchor>
+
+
                 <a
                     target="_blank"
                     href="https://github.com/LangbaseInc/langbase-examples/tree/main/examples/video-wisdom"
@@ -31,6 +45,20 @@ export async function Header() {
                     <IconGitHub />
                     <span className="hidden md:flex">GitHub</span>
                 </a>
+
+                <a
+                    target="_blank"
+                    href="https://langbase.com/examples?q=label:video-wisdom"
+                    rel="noopener noreferrer"
+                    className={cn(buttonVariants({ variant: 'default' }))}
+                >
+                    <IconFork />
+                    <span className="hidden md:flex gap-1">
+                        Fork on <span className="font-bold">Langbase</span>
+                    </span>
+                </a>
+
+
             </div>
         </header>
     )
