bd05ca86e712efc24364928682655810537c6d4b
msaaddev <mrsaadirfan@gmail.com>
2024-08-30 16:13:50 +0200

    📦 NEW: Footer

--- examples/code-alchemist/components/alchemist-page.tsx
+++ examples/code-alchemist/components/alchemist-page.tsx
@@ -5,6 +5,7 @@ import Sandbox from './sandbox';
 import Welcome from './welcome';
 import useLangbase from '@/hooks/use-langbase';
 import { ChatMessage } from './chat-message';
+import Footer from './footer';
 
 export type RecentChat = {
 	pipe: string;
@@ -33,31 +34,34 @@ const AlchemistPage = () => {
 	return (
 		<main
 			className={cn(
-				'flex min-h-screen flex-col items-center gap-y-8 p-4 sm:p-8',
+				'flex min-h-screen flex-col items-center justify-between gap-y-8 sm:gap-y-4 p-4 sm:p-8 sm:pb-5',
 				'min-h-[calc(100vh-100px)]'
 			)}
 		>
-			<Welcome
-				prompt={prompt}
-				isLoading={loading}
-				onSubmit={callLLMs}
-				setPrompt={setPrompt}
-				showSwitch={showSwitch}
-				recentChats={recentChats}
-				showWelcome={showWelcome}
-				showRecentChats={showRecentChats}
-				handleOnClick={handleRecentChatClick}
-			/>
-			<Sandbox
-				prompt={prompt}
-				loading={loading}
-				setPrompt={setPrompt}
-				completion={completion}
-				showPreview={showPreview}
-				showSandbox={showSandbox}
-				improveCode={improveCode}
-			/>
-			<ChatMessage message={completion} showSandbox={showSandbox} />
+			<div className='flex flex-col gap-y-8 w-full'>
+				<Welcome
+					prompt={prompt}
+					isLoading={loading}
+					onSubmit={callLLMs}
+					setPrompt={setPrompt}
+					showSwitch={showSwitch}
+					recentChats={recentChats}
+					showWelcome={showWelcome}
+					showRecentChats={showRecentChats}
+					handleOnClick={handleRecentChatClick}
+				/>
+				<Sandbox
+					prompt={prompt}
+					loading={loading}
+					setPrompt={setPrompt}
+					completion={completion}
+					showPreview={showPreview}
+					showSandbox={showSandbox}
+					improveCode={improveCode}
+				/>
+				<ChatMessage message={completion} showSandbox={showSandbox} />
+			</div>
+			<Footer />
 		</main>
 	);
 };
