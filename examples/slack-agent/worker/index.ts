3d02ce0cc79ad38fab39740e9bf165314c486563
msaaddev <mrsaadirfan@gmail.com>
2025-09-17 23:29:01 +0200

    🐛 FIX: Types

--- examples/slack-agent/worker/index.ts
+++ examples/slack-agent/worker/index.ts
@@ -5,8 +5,14 @@ import { prettyJSON } from 'hono/pretty-json';
 import { secureHeaders } from 'hono/secure-headers';
 import { registerLangbaseEndpoint } from './langbase';
 
+type Bindings = {
+  LANGBASE_API_KEY: string;
+  OPENAI_API_KEY: string;
+  SLACK_BOT_TOKEN: string;
+};
+
 // Create Hono app
-const app = new Hono();
+const app = new Hono<{ Bindings: Bindings }>();
 
 // Middleware
 app.use('*', logger());
@@ -53,7 +59,7 @@ app.onError((err, c) => {
 });
 
 export default {
-  fetch(request: Request, env: Env, ctx: ExecutionContext) {
+  fetch(request: Request, env: Bindings, ctx: any) {
     return app.fetch(request, env, ctx)
   }
 }
